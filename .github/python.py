21e3d79ea9094cef360d5cb2560876b9b10bf5de
MarketingPip <86180097+MarketingPip@users.noreply.github.com>
2022-07-14 05:41:08 -0400

    Testing Old Script

--- .github/python.py
+++ .github/python.py
@@ -62,38 +62,53 @@ var = {}
 varCounter = 0 
 for file in getListOfFiles(dirName):
   with open(file, 'r') as f:
-    file_contents = f.read()	
-    varCounter + 1
     for line in f:
         if ":" in line:
           name, value = line.split('=================END OF SEO SETTINGS============')[0].split(':')  # Needs replaced with regex match 
-          var["name"] = str(value).rstrip() # needs a value added    
-  globals().update(var)
-  file_contents = file_contents
+          var[name] = str(value).rstrip() # needs a value added    
+                 
+          
+          
+    
+globals().update(var)
+
+varCounter = 0 
+for file in getListOfFiles(dirName):
+    with open(file, 'r') as f:
+        varCounter + 1
+        file_contents = f.read()
+        file_contents = file_contents
 
 
-  data = var 
-  Facebook_Meta = ""
-  BlogTitle = "Blog Post"
-  BlogDate = ""
-  SiteTitle = "Site Name"
 
-  Facebook_Meta += """<meta property="og:title" content="Blog Post">"""
-  try:
-           
-      BlogTitle = data["Blog_Title"]
-      BlogDate =  data["BlogDate"]
-      SiteTitle = data["SEO_Title"]
-  except KeyError:
-      pass
-  file_name = outputFolder + Path(file).stem + ".html"
-  try:
-      file_contents = file_contents.split("=================END OF SEO SETTINGS============",1)[1]
-  except:
-      pass
-  try:
-      with codecs.open(file_name, 'w', encoding='utf-8') as f:
-           f.write(f"""<head><title>{SiteTitle}</title>
+
+			
+        data = var 
+        Facebook_Meta = ""
+        BlogTitle = "Blog Post"
+        BlogDate = ""
+        SiteTitle = "Site Name"
+
+        Facebook_Meta += """<meta property="og:title" content="Blog Post">"""
+        try:
+
+           BlogTitle = data["SEO_Title"]
+           BlogDate =  data["BlogDate"]
+           SiteTitle = data["BlogDate"]
+        except KeyError:
+          pass
+
+      
+	
+
+        file_name = outputFolder + Path(file).stem + ".html"
+        try:
+           file_contents = file_contents.split("=================END OF SEO SETTINGS============",1)[1]
+        except:
+          pass
+        try:
+          with codecs.open(file_name, 'w', encoding='utf-8') as f:
+            f.write(f"""<head><title>{SiteTitle}</title>
             <meta charset="utf-8">
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -273,11 +288,8 @@ pre[class*="language-"] {
 	</body>
 	 <script src="https://cdn.jsdelivr.net/gh/MarketingPipeline/Markdown-Tag/markdown-tag-GitHub.js"></script> 
 	""")
-  except IOError: 
-      sys.exit(u'Unable to write to files: {0}'.format(file_contents))  
+        except IOError: 
+         sys.exit(u'Unable to write to files: {0}'.format(file_contents))  
   
 
-         
-                 
-          
-       
+    
