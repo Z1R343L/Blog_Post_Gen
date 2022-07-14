ab5cb10b802dc5215d08690de675c87ddd9fdc4b
MarketingPip <86180097+MarketingPip@users.noreply.github.com>
2022-07-14 09:29:49 -0400

    Try again

--- .github/python.py
+++ .github/python.py
@@ -61,6 +61,7 @@ var = {}
 
 for file in getListOfFiles(dirName):
   with open(file, 'r') as f:
+    file_contents = f.read()
     for line in f:
         if ":" in line:
           name, value = line.split('=================END OF SEO SETTINGS============')[0].split(':')  # Needs replaced with regex match 
@@ -72,28 +73,28 @@ for file in getListOfFiles(dirName):
 globals().update(var)
 
 
-    data = var     
-    print(data)
-    print(data["SEO_Title"])
-    Facebook_Meta = ""
-    BlogTitle = "Blog Post"
-    BlogDate = ""
-    SiteTitle = "Site Name"
-    Facebook_Meta += """<meta property="og:title" content="Blog Post">"""
-    try:
-      BlogTitle = data["SEO_Title"]
-      BlogDate =  data["BlogDate"]
-      SiteTitle = data["BlogDate"]
-    except KeyError:
-      pass
-    file_name = outputFolder + Path(file).stem + ".html"
-    try:
-      file_contents = file_contents.split("=================END OF SEO SETTINGS============",1)[1]
-    except:
-      pass      
-    try:
-      with codecs.open(file_name, 'w', encoding='utf-8') as f:
-        f.write(f"""<head><title>{SiteTitle}</title>
+data = var     
+print(data)
+print(data["SEO_Title"])
+Facebook_Meta = ""
+BlogTitle = "Blog Post"
+BlogDate = ""
+SiteTitle = "Site Name"
+Facebook_Meta += """<meta property="og:title" content="Blog Post">"""
+try:
+  BlogTitle = data["SEO_Title"]
+  BlogDate =  data["BlogDate"]
+  SiteTitle = data["BlogDate"]
+except KeyError:
+  pass
+file_name = outputFolder + Path(file).stem + ".html"
+try:
+  file_contents = file_contents.split("=================END OF SEO SETTINGS============",1)[1]
+except:
+  pass      
+try:
+  with codecs.open(file_name, 'w', encoding='utf-8') as f:
+    f.write(f"""<head><title>{SiteTitle}</title>
             <meta charset="utf-8">
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -273,9 +274,8 @@ pre[class*="language-"] {
 	</body>
 	 <script src="https://cdn.jsdelivr.net/gh/MarketingPipeline/Markdown-Tag/markdown-tag-GitHub.js"></script> 
 	""")
-        data.clear()
-    except IOError:
-      sys.exit(u'Unable to write to files: {0}'.format(file_contents))   
+    data.clear()
+except IOError:
+    sys.exit(u'Unable to write to files: {0}'.format(file_contents))   
 
 
-print(var)
