91fb8bd9285d180a98d080eb363795c6614d620c
MarketingPip <86180097+MarketingPip@users.noreply.github.com>
2022-07-14 06:30:40 -0400

    Update python.py

--- .github/python.py
+++ .github/python.py
@@ -275,7 +275,7 @@ pre[class*="language-"] {
 	</body>
 	 <script src="https://cdn.jsdelivr.net/gh/MarketingPipeline/Markdown-Tag/markdown-tag-GitHub.js"></script> 
 	""")
-    data.clear()
+    globals().clear(var)
 
   except IOError: 
         sys.exit(u'Unable to write to files: {0}'.format(file_contents)) 
