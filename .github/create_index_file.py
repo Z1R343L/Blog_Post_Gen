43d71dfb775abff776f4a5a7a0c146a5e508ace6
MarketingPip <86180097+MarketingPip@users.noreply.github.com>
2022-07-16 03:26:19 -0400

    Update create_index_file.py

--- .github/create_index_file.py
+++ .github/create_index_file.py
@@ -13,7 +13,32 @@ index_file_name = "index.html"
     
 try:
     with codecs.open(index_file_name, 'w', encoding='utf-8') as f:
-        f.write(f"""{index_file_contents}""")
+        f.write(f"""
+        <header>
+   <nav>
+  
+
+</nav>
+
+      <h1>Simply Docs</h1>
+ 
+    </header>
+<main>
+
+{index_file_contents}
+</main>
+<a href="https://github.com/MarketingPipeline/Simply-Docs/archive/refs/heads/main.zip"><button>Download This Template</button></a>
+
+
+
+<footer>
+      <p>Simply Docs was created by <a href="https://github.com/MarketingPipeline/">Marketing Pipeline</a> and is licensed under the MIT license.</p>
+  <small>© 2014 Some company name</small>
+      <address>email@email.com</address>
+    </footer>
+   
+ <script src="https://cdn.jsdelivr.net/gh/MarketingPipeline/Markdown-Tag/markdown-tag.js"></script> 
+        """)
 except IOError:
     sys.exit('Input file does not exist, or has no content.  Exiting')  
 
