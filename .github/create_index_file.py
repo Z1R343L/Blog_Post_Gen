84397872699547e627394ac35587f84efe3b623e
MarketingPip <86180097+MarketingPip@users.noreply.github.com>
2022-07-15 23:34:44 -0400

    Update create_index_file.py

--- .github/create_index_file.py
+++ .github/create_index_file.py
@@ -1,13 +1,9 @@
 import codecs
-#with open(index_template_file) as f:
- # index_template = f.read() 
-
-
 # Open Index File
 index_file_contents = ".github/index.md"
 try:
     with open(index_file_contents, 'r') as f:
-        index_template = f.read()
+        index_file_contents = f.read()
         
         
 except IOError:
@@ -17,6 +13,7 @@ index_file_name = "index.html"
     
 try:
     with codecs.open(index_file_name, 'w', encoding='utf-8') as f:
-        f.write(f"""{index_template}""")
+        f.write(f"""{index_file_contents}""")
 except IOError:
     sys.exit('Input file does not exist, or has no content.  Exiting')  
+
