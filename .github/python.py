cdbea66a1db83b32a52ba7047f805c34fc669335
MarketingPip <86180097+MarketingPip@users.noreply.github.com>
2022-07-16 02:47:26 -0400

    Update python.py

--- .github/python.py
+++ .github/python.py
@@ -25,7 +25,9 @@ pattern = 'Link:(.*?) New_Window:(.*?) Title:(.*?) Position:(.*?) '
 with open(permalinks_file) as f:
   file_contents = f.read()
   for (link, window, title, position) in re.finditer(pattern, file_contents, re.DOTALL):
-   print(link, window, title, position)
+   for value in link:
+    print(link, window, title, position)
+   #print(link, window, title, position)
  # for (link, window, title, position) in re.findall(pattern, file_contents, re.DOTALL):
   #  for value in link:
    #  print(link, window, title, position)
