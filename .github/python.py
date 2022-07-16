e1bb46e3e78cb2771c7eea43ff3c15eee090ff51
MarketingPip <86180097+MarketingPip@users.noreply.github.com>
2022-07-16 02:40:13 -0400

    Update python.py

--- .github/python.py
+++ .github/python.py
@@ -24,7 +24,7 @@ permalinks_file_contents = None
 pattern = re.compile(r'Link:(.*?) New_Window:(.*?) Title:(.*?) Position:(.*?) ')
 with open(permalinks_file) as f:
   file_contents = f.read()
-  for (link, window, title, position) in re.finditer(pattern, file_contents):
+  for (link, window, title, position) in re.findall(pattern, file_contents, re.DOTALL):
     for value in link:
      print(link, window, title, position)
     #  print(link, window, title, position)	
@@ -32,7 +32,6 @@ with open(permalinks_file) as f:
         #for line in file_contents:
                 #for (link, window, title, position) in re.findall(pattern, s):
                  # print(link, window, title, position)		
-			
 #globals().update(PermaLinks)
 #print(PermaLinks)
 #output_file = PermaLinks['Test']
