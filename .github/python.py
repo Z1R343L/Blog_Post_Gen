899c30a1e3318749a94792091d9c1a9e3de58b9a
MarketingPip <86180097+MarketingPip@users.noreply.github.com>
2022-07-16 02:48:24 -0400

    Update python.py

--- .github/python.py
+++ .github/python.py
@@ -24,8 +24,8 @@ permalinks_file_contents = None
 pattern = 'Link:(.*?) New_Window:(.*?) Title:(.*?) Position:(.*?) '
 with open(permalinks_file) as f:
   file_contents = f.read()
-  for (link, window, title, position) in re.finditer(pattern, file_contents, re.DOTALL):
-   for value in link:
+  for line in file_contents:
+   for (link, window, title, position) in re.finditer(pattern, file_contents, re.DOTALL):
     print(link, window, title, position)
    #print(link, window, title, position)
  # for (link, window, title, position) in re.findall(pattern, file_contents, re.DOTALL):
