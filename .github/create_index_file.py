0f0535f4fa4bfa916c1f087412e76c0e4172a9b9
MarketingPip <86180097+MarketingPip@users.noreply.github.com>
2022-07-18 00:45:09 -0400

    Update create_index_file.py

--- .github/create_index_file.py
+++ .github/create_index_file.py
@@ -69,7 +69,7 @@ try:
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-<title>Home | {SiteName}</title>
+<title>Home</title>
   
 <meta name="description" content="A showcase of Simply Docs by MarketingPipeline built using Simple.CSS">
 
