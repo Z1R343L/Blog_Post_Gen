d8d4755175e90cc898d56956ac0a88075c50d48a
MarketingPip <86180097+MarketingPip@users.noreply.github.com>
2022-07-18 01:22:12 -0400

    Apply Fixes

--- .github/python.py
+++ .github/python.py
@@ -125,13 +125,13 @@ def creation_date(path_to_file):
 
 
 for file in getListOfFiles(dirName):
-  file_contents = f.read()
-  file_contents = file_contents
-  try:
-    file_contents = file_contents.split("=================END OF SEO SETTINGS============",1)[1]
-  except:
-    pass
   with open(file, 'r') as f:
+    file_contents = f.read()
+    file_contents = file_contents
+    try:
+      file_contents = file_contents.split("=================END OF SEO SETTINGS============",1)[1]
+    except:
+      pass
     for line in f:
         if ":" in line:
           name, value = line.split('=================END OF SEO SETTINGS============')[0].split(':')  # Needs replaced with regex match 
