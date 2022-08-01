13a5ec4d9d29be48d9d32c0e1711c777e7be8623
MarketingPip <86180097+MarketingPip@users.noreply.github.com>
2022-08-01 19:09:11 -0400

    Update python.py

--- .github/python.py
+++ .github/python.py
@@ -93,10 +93,12 @@ def creation_date(path_to_file):
     else:
         stat = os.stat(path_to_file)
         try:
-          Post_Time = time.strftime('%d %m %Y', time.localtime(stat.st_birthtime))
+	  Date = time.localtime(stat.st_birthtime)	
+          Post_Time = Date.strftime('%d %m %Y')
           return Post_Time
         except AttributeError:
-          Post_Time = time.strftime('%d %m %Y', time.localtime(stat.st_mtime))
+	  Date = time.localtime(stat.st_mtime)	
+          Post_Time = Date.strftime('%d %m %Y')
           return Post_Time
 
 ########################################
