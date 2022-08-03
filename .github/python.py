bdccad34f6da166560e414c441629026a9059314
MarketingPip <86180097+MarketingPip@users.noreply.github.com>
2022-08-03 12:53:42 -0400

    Update python.py

--- .github/python.py
+++ .github/python.py
@@ -732,7 +732,8 @@ for file in getListOfFiles(dirName):
 	      Output_Folder = "assets/" 
       else:   
 	    #   print("outpath: " + path)
-	      Output_Folder = "assets" + path.split("assets/")[1] 
+	      Output_Folder = "assets/" + path.split("assets/")[1] 
+	      print(Path(file).stem + "hello") 
 	      # path.split("/assets/")[1] 
 	
 	
