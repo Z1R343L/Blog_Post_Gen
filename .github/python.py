6f2f06a726d87bce929447a52b7b9f81b6f5a292
MarketingPip <86180097+MarketingPip@users.noreply.github.com>
2022-08-06 23:43:23 -0400

    Update python.py

--- .github/python.py
+++ .github/python.py
@@ -150,12 +150,12 @@ def creation_date(path_to_file, blog_date_format):
         stat = os.stat(path_to_file)
         try:
           Date = stat.st_birthtime
-          print(stat)
+          print(stat + path_to_file)
           Post_Time = datetime.datetime.fromtimestamp(Date, pytz.timezone('US/Eastern')).strftime(blog_date_format)
           return Post_Time
         except AttributeError:
           Date = stat.st_mtime
-          print(stat)
+          print(stat + path_to_file)
           Post_Time = datetime.datetime.fromtimestamp(Date, pytz.timezone('US/Eastern')).strftime(blog_date_format)
           return Post_Time
 
