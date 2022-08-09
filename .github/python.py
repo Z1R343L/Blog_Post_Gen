5196cbfc016af0d2f4018cacd1339e5ce2031f48
MarketingPip <86180097+MarketingPip@users.noreply.github.com>
2022-08-09 04:57:57 -0400

    Update python.py

--- .github/python.py
+++ .github/python.py
@@ -152,7 +152,7 @@ def creation_date(path_to_file, blog_date_format):
           return Post_Time
         except AttributeError:
           # file modification timestamp of a file
-          Date = os.path.getmtime(path_to_file)
+          Date = os.path.getctime(path_to_file)
           Post_Time = datetime.datetime.fromtimestamp(Date, pytz.timezone('US/Eastern')).strftime(blog_date_format)
           return Post_Time
 
